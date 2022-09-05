f19cee9337deb9e92e91154e2f3aeecdd988dbc6
unknown <netzine21@gmail.com>
2022-09-05 12:55:15 +0900

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcom to DevOpS Labs!!!'));
+app.get('/', (req, res) => res.send('This is CI/CD Test!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
